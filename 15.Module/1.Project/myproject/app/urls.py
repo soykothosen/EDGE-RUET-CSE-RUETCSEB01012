6784250627d0aaecdd740ff13e35335ec05600fb
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-04 18:52:43 +0600

    basic routing done

---
+++ 15.Module/1.Project/myproject/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='home'), 
+    #path('add',views.add, name='add'),
+    # path('newpage',views.newpage, name='newpage'),
+    # path('page02',views.page02, name='newpage02'),
+    
+]
