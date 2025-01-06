12929e4998c907e310568c3bdbae1d2906b1383a
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-06 18:51:39 +0600

    upto templete revise

---
+++ 16.Module/1.Project01/batch12project/app01/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('newpage',views.home, name='home'), 
+    path('htmlpage',views.htmlpage, name='htmlpage'),
+    #path('add',views.add, name='add'),
+    # path('newpage',views.newpage, name='newpage'),
+    # path('page02',views.page02, name='newpage02'),
+    
+]
