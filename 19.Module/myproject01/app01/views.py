8f7addcacbeee4d48c4a2e59d46b36ac16d13e8b
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-11 18:54:51 +0600

    basic project setup done

---
+++ 19.Module/myproject01/app01/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# def home(request):
+#     return HttpResponse("Hello World")
+
+def home(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+# def home(request):
+#     return render(request,'home.html',{'name': 'Test Name'})
+
+# def newpage(request):
+#     return render(request,'index.html',{'name': 'Test Name'})
+
+# def add(request):
+#     #var1 = int(request.GET['num1'])
+#     #var2 = int(request.GET['num2'])
+
+#     var1 = int(request.POST['num1'])
+#     var2 = int(request.POST['num2'])
+    
+#     sum = var1 + var2
+
+#     return render(request,'result.html', {'result':sum})
