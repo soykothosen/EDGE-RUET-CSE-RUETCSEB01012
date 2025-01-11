9a87c7152c7a2e42ae6af4e053c64898e0a77a4d
Soykot Hosen <soykothosen1603028@gmail.com>
2025-01-11 19:47:00 +0600

    admin site, join db done

--- 19.Module/myproject01/app01/admin.py
+++ 19.Module/myproject01/app01/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product
+from .models import Category
+from .models import Author, Book
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'price')
+    list_filter = ('published_date', 'author')
+    search_fields = ('title',)
